3ed04507ae54d587479402e92895f2fa84d5756b
FallenGravity <ethanvb55@gmail.com>
2019-08-12 11:46:31 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+function updateDiff() {
+  $.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
+
+    var text = `${data.ticker.last}`
+
+    $(".price").html(text);
+    setTimeout(updateDiff(), 72000000000);
+
+  });
+}
+
+setTimeout(updateDiff(), 72000000000);
+
+function updatePer() {
+  $.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
+
+    var text = `${data.ticker.change}`
+
+    $(".per").html(text);
+    setTimeout(updatePer(), 72000000000);
+
+  });
+}
+
+setTimeout(updatePer(), 72000000000);
+
+var myjson2;
+
+$.getJSON("https://richlist.arhash.xyz/overalls.php", function(data) {
+  myjson2 = `${data.supply}`;
+});
+
+var myjson;
+
+$.getJSON("https://api.coingecko.com/api/v3/coins/bitcoin?tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true", function(data) {
+  myjson = `${data.market_data.current_price.usd}`;
+});
+
+function updateDiff2() {
+  $.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
+
+    var text = (`${data.ticker.last}` * 16700439);
+
+
+    $(".price2").html(text);
+    setTimeout(updateDiff2(), 72000000000);
+
+  });
+}
+
+setTimeout(updateDiff2(), 72000000000);
+
+function updateDiff3() {
+  $.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
+
+    var text = (`${data.ticker.last}` * 16700439)  * myjson;
+
+
+    $(".price3").html(text);
+    setTimeout(updateDiff3(), 72000000000);
+
+  });
+}
+
+setTimeout(updateDiff3(), 72000000000);
