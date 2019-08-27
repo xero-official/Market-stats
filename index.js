b17ba7b4e10bee644f51fe7f8d914b2cc0a305ea
Ethan van Ballegooyen <ethanvb55@gmail.com>
2019-08-27 22:28:12 +0200

    Added Dev Balance and Locked Collateral stats.

--- index.js
+++ index.js
@@ -1,217 +1,242 @@
 //Currenct BTC Price
-function updateDiff() {
-  $.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
+$.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
 
-    var text = `${data.ticker.last}`
+  var text = `${data.ticker.last}`
 
-    $(".price").html(text);
-    setTimeout(updateDiff(), 72000000000);
+  $(".price").html(text);
 
-  });
-}
-
-setTimeout(updateDiff(), 72000000000);
-
-//Pulls % Change
-function updatePer() {
-  $.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
-
-    var text = `${data.ticker.change}`
-
-    $(".per").html(text);
-    setTimeout(updatePer(), 72000000000);
-
-  });
-}
-
-setTimeout(updatePer(), 72000000000);
-
-//Pulls Circ Supply
-//var myjson2;
-
-//$.getJSON("https://richlist.arhash.xyz/overalls.php", function(data) {
-//  myjson2 = `${data.supply}`;
-//});
-
-// Pulls BTC Price
-var myjson;
-
-$.getJSON("https://api.coingecko.com/api/v3/coins/bitcoin?tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true", function(data) {
-  myjson = `${data.market_data.current_price.usd}`;
 });
-
-//BTC Value MCAP
-function updateDiff2() {
-  $.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
-
-    var text = (`${data.ticker.last}` * 22942685);
-    var fix = text.toFixed(4);
-
-
-    $(".price2").html(fix);
-    setTimeout(updateDiff2(), 72000000000);
-
-  });
-}
-
-setTimeout(updateDiff2(), 72000000000);
-
-//USD Value MCAP
-function updateDiff3() {
-  $.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
-
-    var text = (`${data.ticker.last}` * 22942685) * myjson;
-    var usdm = text.toFixed(3);
-
-
-    $(".price3").html(usdm);
-    setTimeout(updateDiff3(), 72000000000);
-
-  });
-}
-
-setTimeout(updateDiff3(), 72000000000);
-
-//Chain Node BTC
-function updateChain() {
-  $.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
-
-    var text = `${data.ticker.last}` * 5000;
-    var fix = text.toFixed(5);
-
-    $(".chain").html(fix);
-    setTimeout(updateChain(), 72000000000);
-
-  });
-}
-
-setTimeout(updateChain(), 72000000000);
-
-//Chain Node USD
-function updateChain1() {
-  $.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
-
-    var text = (`${data.ticker.last}` * 5000) * myjson;
-    var usdm = text.toFixed(3);
-
-    $(".chainu").html(usdm);
-    setTimeout(updateChain1(), 72000000000);
-
-  });
-}
-
-setTimeout(updateChain1(), 72000000000);
-
-//XERO Node BTC
-function updateXERO() {
-  $.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
-
-    var text = `${data.ticker.last}` * 20000;
-    var fix = text.toFixed(5);
-
-    $(".xero").html(fix);
-    setTimeout(updateXERO(), 72000000000);
-
-  });
-}
-
-setTimeout(updateXERO(), 72000000000);
-
-//XERO Node USD
-function updateXERO1() {
-  $.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
-
-    var text = (`${data.ticker.last}` * 20000) * myjson;
-    var usdm = text.toFixed(3);
-
-    $(".xerou").html(usdm);
-    setTimeout(updateXERO1(), 72000000000);
-
-  });
-}
-
-setTimeout(updateXERO1(), 72000000000);
-
-//Link Node BTC
-function updateLink() {
-  $.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
-
-    var text = `${data.ticker.last}` * 40000;
-    var fix = text.toFixed(5);
-
-    $(".link").html(fix);
-    setTimeout(updateLink(), 72000000000);
-
-  });
-}
-
-setTimeout(updateLink(), 72000000000);
-
-//Link Node USD
-function updateLink1() {
-  $.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
-
-    var text = (`${data.ticker.last}` * 40000) * myjson;
-    var usdm = text.toFixed(3);
-
-    $(".linku").html(usdm);
-    setTimeout(updateLink1(), 72000000000);
-
-  });
-}
-
-setTimeout(updateLink1(), 72000000000);
-
-//Super Node BTC
-function updateSuper() {
-  $.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
-
-    var text = `${data.ticker.last}` * 80000;
-    var fix = text.toFixed(5);
-
-    $(".super").html(fix);
-    setTimeout(updateSuper(), 72000000000);
-
-  });
-}
-
-setTimeout(updateSuper(), 72000000000);
-
-//Super Node USD
-function updateSuper1() {
-  $.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
-
-    var text = (`${data.ticker.last}` * 80000) * myjson;
-    var usdm = text.toFixed(3);
-
-    $(".superu").html(usdm);
-    setTimeout(updateSuper1(), 72000000000);
-
-  });
-}
-
-setTimeout(updateSuper1(), 72000000000);
-
-//Currenct BTC Price
-function updatePusd() {
-  $.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
-
-    var text = `${data.ticker.last}` * myjson;
-    var usd = text.toFixed(5);
-
-    $(".priceu").html(usd);
-    setTimeout(updatePusd(), 72000000000);
-
-  });
-}
-
-setTimeout(updatePusd(), 72000000000);
 
 //calc btc
 var myjson3;
 
 $.getJSON("https://tradecx.io/api/tickers/xerobtc", function(data) {
   myjson3 = `${data.ticker.last}`;
+
+  $.getJSON('https://blocks.xerom.org/api?module=account&action=balance&address=0x93b7a5c74793dcba765a1dd163e1744622306651', function(result) {
+
+    var amount = `${result.result}` / 1000000000000000000;
+    var btcv = amount * myjson3;
+    var value = btcv.toFixed(4);
+
+    console.log(value + " Final Answer in BTC");
+    console.log(amount + " XERO");
+    console.log(btcv + " XERO * Current BTC Price")
+
+    $(".locks").html(value);
+
+  });
+
+  $.getJSON('https://blocks.xerom.org/api?module=account&action=balance&address=0xe44389c26fdeb581dea7df91efd0665a7cd404c1', function(result) {
+
+    var amountl = `${result.result}` / 1000000000000000000;
+    var btcvl = amountl * myjson3;
+    var valuel = btcvl.toFixed(4);
+
+    console.log(valuel + " Final Answer in BTC");
+    console.log(amountl + " XERO");
+    console.log(btcvl + " XERO * Current BTC Price")
+
+    $(".lockl").html(valuel);
+
+  });
+
+  $.getJSON('https://blocks.xerom.org/api?module=account&action=balance&address=0xc46cc53b8f09fe6f4eb6b6df8ad5c6fe5da6638b', function(result) {
+
+    var amountx = `${result.result}` / 1000000000000000000;
+    var btcvx = amountx * myjson3;
+    var valuex = btcvx.toFixed(4);
+
+    console.log(valuex + " Final Answer in BTC");
+    console.log(amountx + " XERO");
+    console.log(btcvx + " XERO * Current BTC Price")
+
+    $(".lockx").html(valuex);
+
+  });
+
+  $.getJSON('https://blocks.xerom.org/api?module=account&action=balance&address=0x3717ad55666577eb92fca3e5f9f71958bd60c620', function(result) {
+
+    var amountc = `${result.result}` / 1000000000000000000;
+    var btcvc = amountc * myjson3;
+    var valuec = btcvc.toFixed(4);
+
+    console.log(valuec + " Final Answer in BTC");
+    console.log(amountc + " XERO");
+    console.log(btcvc + " XERO * Current BTC Price")
+
+    $(".lockc").html(valuec);
+
+  });
+
+  $.getJSON('https://blocks.xerom.org/api?module=account&action=balance&address=0xb69b9216b5089dc3881a4e38f691e9b6943dfa11', function(result) {
+
+    var amountd = `${result.result}` / 1000000000000000000;
+    var btcvd = amountd * myjson3;
+    var valued = btcvd.toFixed(4);
+
+    console.log(valued + " Final Answer in BTC");
+    console.log(amountd + " XERO");
+    console.log(btcvd + " XERO * Current BTC Price")
+
+    $(".lockd").html(valued);
+
+  });
+
+  console.log(myjson3 + ' TCX BTC Price');
+
+});
+
+//Pulls % Change
+$.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
+
+  var text = `${data.ticker.change}`
+
+  $(".per").html(text);
+
+});
+
+// Pulls BTC Price
+var myjson;
+
+$.getJSON("https://api.coingecko.com/api/v3/coins/bitcoin?tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true", function(data) {
+  myjson = `${data.market_data.current_price.usd}`;
+
+  var myjson3;
+
+  $.getJSON("https://tradecx.io/api/tickers/xerobtc", function(data) {
+    myjson3 = `${data.ticker.last}`;
+
+    $.getJSON('https://blocks.xerom.org/api?module=account&action=balance&address=0xb69b9216b5089dc3881a4e38f691e9b6943dfa11', function(result) {
+
+      var amountdu = `${result.result}` / 1000000000000000000;
+      var btcvdu = amountdu * myjson3;
+      var valuedu = btcvdu * myjson;
+      var final = valuedu.toFixed(3)
+
+      console.log(valuedu + " Final Answer in BTC");
+      console.log(amountdu + " XERO");
+      console.log(btcvdu + " XERO * Current BTC Price")
+
+      $(".lockdu").html(final);
+
+    });
+
+  });
+
+  console.log(myjson + " BTC USD Price");
+});
+
+//BTC Value MCAP
+$.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
+
+  var text = (`${data.ticker.last}` * 22942685);
+  var fix = text.toFixed(4);
+
+
+  $(".price2").html(fix);
+
+});
+
+//USD Value MCAP
+$.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
+
+  var text = (`${data.ticker.last}` * 22942685) * myjson;
+  var usdm = text.toFixed(3);
+
+
+  $(".price3").html(usdm);
+
+});
+
+//Chain Node BTC
+$.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
+
+  var text = `${data.ticker.last}` * 5000;
+  var fix = text.toFixed(5);
+
+  $(".chain").html(fix);
+
+});
+
+//Chain Node USD
+$.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
+
+  var text = (`${data.ticker.last}` * 5000) * myjson;
+  var usdm = text.toFixed(3);
+
+  $(".chainu").html(usdm);
+
+});
+
+//XERO Node BTC
+$.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
+
+  var text = `${data.ticker.last}` * 20000;
+  var fix = text.toFixed(5);
+
+  $(".xero").html(fix);
+
+});
+
+//XERO Node USD
+$.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
+
+  var text = (`${data.ticker.last}` * 20000) * myjson;
+  var usdm = text.toFixed(3);
+
+  $(".xerou").html(usdm);
+
+});
+
+//Link Node BTC
+$.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
+
+  var text = `${data.ticker.last}` * 40000;
+  var fix = text.toFixed(5);
+
+  $(".link").html(fix);
+
+});
+
+//Link Node USD
+$.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
+
+  var text = (`${data.ticker.last}` * 40000) * myjson;
+  var usdm = text.toFixed(3);
+
+  $(".linku").html(usdm);
+
+});
+
+//Super Node BTC
+$.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
+
+  var text = `${data.ticker.last}` * 80000;
+  var fix = text.toFixed(5);
+
+  $(".super").html(fix);
+
+});
+
+//Super Node USD
+$.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
+
+  var text = (`${data.ticker.last}` * 80000) * myjson;
+  var usdm = text.toFixed(3);
+
+  $(".superu").html(usdm);
+
+});
+
+//Currenct BTC Price
+$.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
+
+  var text = `${data.ticker.last}` * myjson;
+  var usd = text.toFixed(5);
+
+  $(".priceu").html(usd);
+
 });
 
 function calc() {
@@ -221,17 +246,8 @@ function calc() {
 }
 
 //calc usd
-
 function calc2() {
   var xeroamount2 = parseFloat(document.getElementById('xeroamount2').value); {
     document.getElementById('result2').value = (xeroamount2 * myjson3) * myjson;
   }
 }
-
-//BR BTC
-$.getJSON('https://tradecx.io/api/tickers/xerobtc', function(data) {
-
-  var text = `${data.ticker.last}` * 24;
-
-  $(".brbtc").html(text);
-});
